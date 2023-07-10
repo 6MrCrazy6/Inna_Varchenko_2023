25adca9d5392a1756e8766da42eb8c0595e1ebbe
Inna Varchenko <117535858+6MrCrazy6@users.noreply.github.com>
2023-07-10 18:00:27 +0300

    Add files via upload

---
+++ Lesson_1/task_3/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Enter first string");
+string str1 = Console.ReadLine();
+Console.WriteLine("Enter second string");
+string str2 = Console.ReadLine();
+
+if (str1.Length > str2.Length)
+{
+    string result = str1 + " " + str2;
+    Console.WriteLine(result);
+}
+
+if (str1.Length < str2.Length)
+{
+    string[] result = str2.Split(str1[0]);
+    foreach (string s in result)
+    {
+        Console.WriteLine(s);
+    }
+}
+
+if (str1.Length == str2.Length)
+{
+    Console.WriteLine("Result: ");
+}
