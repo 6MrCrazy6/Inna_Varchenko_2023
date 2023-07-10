19a6a37b66049578af8759f762403836e85ef7a6
Inna Varchenko <117535858+6MrCrazy6@users.noreply.github.com>
2023-07-10 17:19:56 +0300

    Add files via upload

---
+++ Lesson_1/task_2/Program.cs
@@ -0,0 +1,20 @@
+﻿Random rnd = new Random();
+List<int> list = new List<int>(20);
+for (int i = 0; i <= 10; i++)
+{
+    list.Add(rnd.Next(0, 10));
+}
+Console.WriteLine(string.Join(", ", list));
+
+Console.WriteLine("Enter number:");
+int number = Convert.ToInt32(Console.ReadLine());
+for (int i = 0; i < list.Count; i++)
+{
+    if (list[i] == number)
+    {
+        list.Insert(i + 1, number);
+        i++;
+    }
+
+}
+Console.WriteLine(string.Join(", ", list));
